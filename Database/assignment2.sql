2f95685f1266a83547f86c2353c0aecb2e1b1629
KyerraBright <Kyerra@comcast.net>
2023-11-09 15:50:10 -0700

    week2

---
+++ Database/assignment2.sql
@@ -0,0 +1,32 @@
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+) VALUES (
+    'Tony',
+    'Stark',
+    'tony@starkent.com',
+    'Iam1ronM@n'
+);
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'large interiors')
+WHERE inv_id = 10;
+
+SELECT
+    i.inv_make,
+    i.inv_model,
+    c.classification_name
+FROM
+    public.inventory i
+INNER JOIN
+    public.classification c ON i.classification_id = c.classification_id
+WHERE
+    c.classification_name = 'Sport';
+
+UPDATE public.inventory
+SET
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
